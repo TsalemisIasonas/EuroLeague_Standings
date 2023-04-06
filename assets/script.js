e9a077fc5297ddb8ce3e212c8dfb64fdf8e2bb24
Iasonas Tsalemis <87096813+TsalemisIasonas@users.noreply.github.com>
2023-04-06 11:37:04 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+function createTable(data) {
+  let table = document.createElement("table");
+  let header = table.createTHead();
+  let row = header.insertRow();
+  let headers = Object.keys(data[0]);
+  headers.forEach((headerText) => {
+    let th = document.createElement("th");
+    let text = document.createTextNode(headerText);
+    th.appendChild(text);
+    row.appendChild(th);
+  });
+  let tbody = table.createTBody();
+  data.forEach((rowData) => {
+    let row = tbody.insertRow();
+    Object.values(rowData).forEach((cellData) => {
+      let cell = row.insertCell();
+      let text = document.createTextNode(cellData);
+      cell.appendChild(text);
+    });
+  });
+  return table;
+}
+
+async function get_data() {
+  return eel.get_data()();
+}
+
+async function loadStandings() {
+  const standings = await get_data();
+  const table = createTable(standings);
+  document.getElementById("standings-table").appendChild(table);
+}
+
+loadStandings();
